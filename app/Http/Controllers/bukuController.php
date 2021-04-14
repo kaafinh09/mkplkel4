41c987090532cee378c390f3913227e2ce632589
kaafinh09 <kaafinh09@gmail.com>
2021-04-14 21:12:07 +0700

    fixed

--- app/Http/Controllers/bukuController.php
+++ app/Http/Controllers/bukuController.php
@@ -44,19 +44,7 @@ class bukuController extends Controller
             return redirect("/welcome");
     }
 
-    public function update(Request $req)
-    {
-        
-            $update = buku::find($req->id);
-            $update->judul=$req->judul;
-            $update->pengarang=$req->pengarang;
-            $update->tahun=$req->tahun;
-            $update->penerbit=$req->penerbit;
-            $update->stok=$req->stock;
-            $update->save();
-
-            return redirect("/welcome");
-    }
+    
 
 
 
